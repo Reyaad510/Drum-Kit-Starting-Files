30fc6e5f03d6a4b5aef9dcf007d21e26e5efbc8a
Nicholas Reyaad Rafeek <45085276+Reyaad510@users.noreply.github.com>
2023-11-26 23:11:19 -0500

    Added one sound and background imgs for all keys

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 
 
+
+// Add an event listener
+// querySelectorAll then pass in ".drum" to targer that class which are on the buttons, then when click on button will call the handleClick() function
+
+
 let numberOfDrumButtons = document.querySelectorAll(".drum").length;
 
 const handleClick = () => {
-    alert("I got clicked!");
+    let audio = new Audio("sounds/tom-1.mp3");
+    audio.play();
+
     
     }
 
-
-// Add an event listener
-
-// querySelectorAll then pass in ".drum" to targer that class which are on the buttons, then when click on button will call the handleClick() function
 for(let i = 0; i < numberOfDrumButtons; i++) {
 
     document.querySelectorAll(".drum")[i].addEventListener("click", handleClick);
@@ -22,3 +25,28 @@ for(let i = 0; i < numberOfDrumButtons; i++) {
 
 
 
+
+
+
+// Higher order functions are functions that can take other functions as inputs.
+
+/*
+function add(num1, numm2) {
+    return num1 + num2;
+}
+
+function multiply(num1, numm2) {
+    return num1  * num2;
+}
+
+function calculator(num1, numm2, operator){
+    return operator(num1,num2);
+}
+
+calculator(num1,num2,add);
+
+debugger; -> in console of website walks through step by step to figure out an error
+
+
+
+*/
