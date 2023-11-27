f6ade5c73fd60ffdf217b874ae1f102c9a976133
Nicholas Reyaad Rafeek <45085276+Reyaad510@users.noreply.github.com>
2023-11-26 22:29:14 -0500

    Added functionality to all buttons

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 
-// Add an event listener
-document.querySelector("button").addEventListener("click", handleClick);
+
+let numberOfDrumButtons = document.querySelectorAll(".drum").length;
 
 const handleClick = () => {
-alert("I got clicked!");
+    alert("I got clicked!");
+    
+    }
 
+
+// Add an event listener
+
+// querySelectorAll then pass in ".drum" to targer that class which are on the buttons, then when click on button will call the handleClick() function
+for(let i = 0; i < numberOfDrumButtons; i++) {
+
+    document.querySelectorAll(".drum")[i].addEventListener("click", handleClick);
+
+    
 }
 
 
-alert("hi")
+
+
+
